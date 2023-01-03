a79654e28dc632eb0f904bab2213f25ff4828a9a
刘磊 <liulei01@bilibili.com>
2023-01-03 21:24:43 +0800

    feat:update example data

--- src/main/resources/sql/example_data.sql
+++ src/main/resources/sql/example_data.sql
@@ -6,6 +6,6 @@ INSERT INTO family_member_information (user_id,nickname,username,appellation,sco
 ('f639b993-0513-4f0d-bd1e-cd5d3684dba2','mama','小王','妈妈',5,'c66e12c1-ef06-423e-b447-3be021084d64',0,'2023-01-02 12:12:23','2023-01-02 12:12:23');
 
 INSERT INTO family_member_score_detail (user_id,change_description,work_user_id,score,is_delete,create_time,update_time) VALUES
-('f639b993-0513-4f0d-bd1e-cd5d3684dba2','vina让richur洗车厘子','64c4ff60-b0f5-4121-b025-f63c3e98d1a6',1,0,'2023-01-01 15:20:13','2023-01-01 15:20:13'),
+('f639b993-0513-4f0d-bd1e-cd5d3684dba2','mama让baba洗车厘子','64c4ff60-b0f5-4121-b025-f63c3e98d1a6',1,0,'2023-01-01 15:20:13','2023-01-01 15:20:13'),
 ('f639b993-0513-4f0d-bd1e-cd5d3684dba2','剪刀石头布获取','',6,0,'2023-01-01 15:16:25','2023-01-01 15:16:25'),
 ('64c4ff60-b0f5-4121-b025-f63c3e98d1a6','剪刀石头布获取','',4,0,'2023-01-01 15:16:25','2023-01-01 15:16:25');
